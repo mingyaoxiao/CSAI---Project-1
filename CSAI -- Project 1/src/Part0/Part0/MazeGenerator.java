b61e881c4966346c11a1f7203c333adb8d87c84c
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-02-12 17:24:02 -0500

    Created a stub for createMazes. Demonstrates how to use nameFunc.

--- CSAI -- Project 1/src/Part0/Part0/MazeGenerator.java
+++ CSAI -- Project 1/src/Part0/Part0/MazeGenerator.java
@@ -4,6 +4,8 @@ import java.util.function.Function;
 
 public class MazeGenerator {
 	public static void createMazes(int n, Function<Integer,String> nameFunc, String folderLocation, int sizeX, int sizeY) {
-		return;
+		for(int i = 0; i < n; i++) {
+			System.out.println("Created the file '" + nameFunc.apply(i) + Maze.mazeExtension +  "' in the directory '"+folderLocation+"' with size of " + sizeX + " by " + sizeY);
+		}
 	}
 }
