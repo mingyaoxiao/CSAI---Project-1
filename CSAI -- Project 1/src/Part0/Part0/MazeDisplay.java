4e061637988ba3b711d8f1ea004efb8a86faf4a2
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-02-12 15:48:08 -0500

    Introduced class to bundle more information for each maze display.

---
+++ CSAI -- Project 1/src/Part0/Part0/MazeDisplay.java
@@ -0,0 +1,10 @@
+package Part0;
+
+public class MazeDisplay {
+	String fileName;
+	Maze maze;
+	public MazeDisplay(String fileName, Maze maze) {
+		this.fileName = fileName;
+		this.maze = maze;
+	}
+}
