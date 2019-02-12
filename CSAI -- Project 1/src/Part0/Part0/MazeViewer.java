1beb2e1723757481f2fe5b4313b48b5ed8fb9f03
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-02-12 00:37:36 -0500

    Implemented loadMazes and loadThenAddMaze

--- CSAI -- Project 1/src/Part0/Part0/MazeViewer.java
+++ CSAI -- Project 1/src/Part0/Part0/MazeViewer.java
@@ -3,26 +3,33 @@ import java.io.File;
 import java.util.*;
 public class MazeViewer {
 	List<Maze> loadedMazes;
+	ListIterator<Maze> mazeIterator;
+	
+	public MazeViewer() {
+		loadedMazes = new ArrayList<Maze>();
+		mazeIterator = loadedMazes.listIterator();
+	}
 	
 	public void loadThenAddMaze(String fileLocation) {
-		
+		Maze readMaze = Maze.loadMaze(fileLocation);
+		mazeIterator.add(readMaze);
 	}
 	
 	public void loadMazes(String folderLocation) {
-	//Will call loadAndAddMaze for each maze file it encounters
 		File folder = new File("F:/Path");
 		if(!folder.isDirectory()) return;
 	    File[] files = folder.listFiles();
 	    for (File file : files)
 	    {
-	        if (file.isFile())
+	        if (file.isFile() && FileHelper.getExtension(file.getName()) == Maze.mazeExtension)
 	        {
-	            System.out.println(file.getName());
+	            loadThenAddMaze(file.getPath());
 	        }
 	    }
 	}
 	
 	public Maze nextDisplay() {
+		if(mazeIterator.hasNext())return mazeIterator.next();
 		return null;
 	}
 }
