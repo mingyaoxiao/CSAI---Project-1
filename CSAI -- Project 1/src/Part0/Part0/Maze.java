2b8f9ed1d16951627fc093287a9682aed30f6946
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-02-12 12:26:38 -0500

    Fixed issue where saving or loading wouldnt append the extension

--- CSAI -- Project 1/src/Part0/Part0/Maze.java
+++ CSAI -- Project 1/src/Part0/Part0/Maze.java
@@ -13,11 +13,19 @@ public class Maze implements Serializable{
 		return null;
 	}
 	
-	public Maze(int sizeX, int sizeY) {
+	private Maze(int sizeX, int sizeY) {
 		size = new int[] {sizeX, sizeY};
 		initializeBlankMaze();
 		renderCells();
 	}
+	
+	public static Maze createNewMaze(int sizeX, int sizeY) {
+		Maze m = new Maze(sizeX, sizeY);
+		m.initializeBlankMaze();
+		m.renderCells();
+		return m;
+	}
+	
 	public Object getRender() {
 		renderCells();
 		return this.renderedView;
@@ -27,9 +35,12 @@ public class Maze implements Serializable{
 	}
 	
 	private void initializeBlankMaze() {
+		cells = new Cell[size[0]][size[1]];
 		for(int i = 0; i < size[0]; i++) {
 			for(int j = 0; j < size[1]; j++) {
-				cells[i][j] = new Cell(this);
+				if(this != null) {
+					cells[i][j] = new Cell(this);
+				}
 			}
 		}
 	}
@@ -37,7 +48,7 @@ public class Maze implements Serializable{
 	public static Maze loadMaze(String filePath) {
 		ObjectInputStream mazeReader = null;
 		try {
-			mazeReader = new ObjectInputStream(new FileInputStream(filePath));
+			mazeReader = new ObjectInputStream(new FileInputStream(filePath + "." + Maze.mazeExtension));
 			Maze readMaze = (Maze) mazeReader.readObject();
 			mazeReader.close();
 			return readMaze;
@@ -55,7 +66,7 @@ public class Maze implements Serializable{
 	public void saveMaze(String filePath) {
 		ObjectOutputStream mazeWriter = null;
 		try {
-			mazeWriter = new ObjectOutputStream(new FileOutputStream(filePath));
+			mazeWriter = new ObjectOutputStream(new FileOutputStream(filePath+ "." + Maze.mazeExtension));
 			mazeWriter.writeObject(this);
 			mazeWriter.close();
 		} catch (Exception e) {
