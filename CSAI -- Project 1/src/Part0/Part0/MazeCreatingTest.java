5af5ce089e3b66ccba6a8aac251a75b291e0bd86
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-02-12 14:03:07 -0500

    A demonstration of the rendering capability

--- CSAI -- Project 1/src/Part0/Part0/MazeCreatingTest.java
+++ CSAI -- Project 1/src/Part0/Part0/MazeCreatingTest.java
@@ -1,8 +1,34 @@
 package Part0;
 
+import static Part0.MoveDirection.*;
+
 public class MazeCreatingTest {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//generateMaze(5,5);
+		Maze m = Maze.loadMaze(FileHelper.baseUrl + "testMaze3");
+		m.toString();
+		Cell c = m.getCellAtCoordinates(2,3);
+		c.setAsBlocked();
+		c = c.moveToNeighbor(Up);
+		c.setAsBlocked();
+		c = c.moveToNeighbor(Left);
+		c.setAsBlocked();
+		c = c.moveToNeighbor(Left);
+		c.setAsBlocked();
+		c = c.moveToNeighbor(Down);
+		c.setAsBlocked();
+		c = c.moveToNeighbor(Down);
+		c.setAsBlocked();
+		c = c.moveToNeighbor(Down);
+		c.setAsBlocked();
+		c = c.moveToNeighbor(Down);
+		String renderString = m.getRender().toString();
+		if(c == null) renderString += "\n Boundary reached";
+		System.out.println(renderString);
+	}
+	
+	private static void generateMaze(int sizeX, int sizeY) {
+		Maze m = Maze.createNewMaze(sizeY, sizeY);
+		m.saveMaze(FileHelper.baseUrl + "testMaze3");
 	}
 }
