4d24645a20a971ed7aea9e0f5d87db3641b5772e
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-02-12 14:02:54 -0500

    Finished implementation of text based renderer.

--- CSAI -- Project 1/src/Part0/Part0/MazeRenderer.java
+++ CSAI -- Project 1/src/Part0/Part0/MazeRenderer.java
@@ -1,7 +1,22 @@
 package Part0;
+import static Part0.CellStatus.*;
 
 public class MazeRenderer {
 	public static Object renderMaze(Maze maze) {
-		return maze.toString();
+		int sizeX = maze.size[0];
+		int sizeY = maze.size[1];
+		StringBuilder renderString = new StringBuilder();
+		
+		for(int i = 0; i < sizeX; i++) {
+			for(int j = 0; j < sizeY; j++) {
+				Cell currentCell = maze.getCellAtCoordinates(new int[] {i,j});
+				if (currentCell.status == Blocked) renderString.append(" \u25A0 ");
+				else if (currentCell.status == Unblocked) renderString.append(" \u25A1 ");
+			}
+			renderString.append("\n");
+		}
+		
+		
+		return renderString;
 	}
 }
