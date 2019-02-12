32d2168898a8e5a9722a966fbce76b08f71112e0
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-02-12 08:58:39 -0500

    Added MazeGenerator stub

---
+++ CSAI -- Project 1/src/Part0/Part0/MazeGenerator.java
@@ -0,0 +1,9 @@
+package Part0;
+
+import java.util.function.Function;
+
+public class MazeGenerator {
+	public static void createMazes(int n, Function<Integer,String> nameFunc, String folderLocation, int sizeX, int sizeY) {
+		return;
+	}
+}
