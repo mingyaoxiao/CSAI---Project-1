d60e243dca7098b6658937ae2f7fd6bfaae36b12
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-02-21 23:16:43 -0500

    Step 1 and 2 in addToVisualization func

--- CSAI -- Project 1/part2/RepeatedAStarAgent.java
+++ CSAI -- Project 1/part2/RepeatedAStarAgent.java
@@ -1,17 +1,21 @@
 package part2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import static Part0.CellStatus.*;
 
 import Part0.Maze;
 
 public class RepeatedAStarAgent {
 	Game game;
-	
+	List<L_Cell> prevPath;
 	public RepeatedAStarAgent(Game game) {
 		this.game = game;
+		prevPath = new ArrayList<L_Cell>();
 	}
 	
-	public List<String> ComputePath(L_Cell curr){
+	public List<String> ComputePath(L_Cell agentStart, L_Cell agentEnd){
 		return null;
 	}
 	
@@ -22,12 +26,29 @@ public class RepeatedAStarAgent {
 	public GameVisualization Run() {
 		L_Grid agent = new L_Grid(this.game.trueMaze, this);
 		agent.aStar();
+		int numberOfExpandedCells = agent.numberOfExpandedCells;
 		return null;
 	}
-
-	public void addToVisualization(L_Cell curr, List<int[]> newBlockedCells) {
+	
+	public void addToVisualization(L_Cell agentStart, L_Cell agentEnd, List<int[]> newBlockedCells) {
 		// TODO Auto-generated method stub
-		List<String> path = this.ComputePath(curr);
-		Execute(path);
+		/*List<String> path = this.ComputePath(agentStart, agentEnd);
+		Execute(path);*/
+		
+		// 1. Reset cellstatus from agentvisited to unblocked.
+		Iterator<L_Cell> it = prevPath.iterator();
+		while(it.hasNext()) {
+			L_Cell cur = it.next();
+			game.awareMaze.getCellAtCoordinates(cur.xCoor, cur.yCoor).status = Unblocked;
+		}
+		// 2. Set the appropriate cells from unblocked to agentvisited.
+		L_Cell ptr = agentStart;
+		L_Cell prev = ptr;
+		while(ptr != null) {
+			L_Cell cur = ptr;
+			game.awareMaze.getCellAtCoordinates(cur.xCoor, cur.yCoor).status = AgentVisited;
+		}
+		// 3. Set the appropriate cells from unblocked to blocked.
+		// 4. Render the aware maze and add it to the visualization history. 
 	}
 }
